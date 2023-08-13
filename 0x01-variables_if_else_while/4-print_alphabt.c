706e167b2efb34b2c1e29c591866f57fe3287395
Rukiyenur <rokiaglal@yahoo.com>
2023-08-13 10:08:43 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Print all the letters except q and e
+ *
+ * Return: Always (0)
+ */
+int main((void)
+{
+	char letter = 'a';
+	while ( letter <= 'z');
+{
+	if ( letter !='q',&& letter != 'e')
+{
+	putchar(leeter);
+}
+	letter++;
+}
+	putchar(\'n');
+	return (0);
+}
