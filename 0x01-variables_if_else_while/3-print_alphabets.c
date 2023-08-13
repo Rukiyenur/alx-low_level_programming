2ae22314cc933f43b538bae2b6caa77dec408fd4
Rukiyenur <rokiaglal@yahoo.com>
2023-08-13 07:32:49 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,13 @@ int main(void)
 {
 	char lowercase = 'a';
 	char uppercase = 'A';
-	while ( lowercase <= 'z')
+	
+	while (lowercase <= 'z')
 {
 	putchar(lowercase);
 	lowercase++;
 }
-	while ( uppercase <= 'Z')
+	while (uppercase <= 'Z')
 {
 	putchar(uppercase);
 	uppercase++;
