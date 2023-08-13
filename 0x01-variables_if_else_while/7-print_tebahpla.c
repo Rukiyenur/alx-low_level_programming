baea147982418f431a5da955c0aa7f3258e127df
Rukiyenur <rokiaglal@yahoo.com>
2023-08-13 13:16:06 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stido.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the lowercase alphabet in reverse
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int letter = 'z';
+
+	while (letter <= 'a')
+{
+	putchar(letter);
+}
+	letter--;
+	putchar('/n');
+	return (0);
+}
