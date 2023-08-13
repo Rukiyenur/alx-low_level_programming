c1a13829a558fe9a0dc2c1b2f5890154f467c7d1
Rukiyenur <rokiaglal@yahoo.com>
2023-08-13 11:20:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stido.h>
+/**
+ * main - entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+{
+	putchar(digit + '0');
+        digit++;
+}
+	putchar('\n');
+
+	return (0);
+}
