1a88e4eaa265a124d7b7ce5769cab22b4a764c0e
Rukiyenur <rokiaglal@yahoo.com>
2023-08-16 13:56:23 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -3,20 +3,20 @@
 /**
  * main - check the code
  *
- *Description: function that checks for lowercase character
+ * Description: function that checks for lowercase character
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
