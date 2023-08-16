c95e569f5cd48d030093f0cacf1105abbdad447a
Rukiyenur <rokiaglal@yahoo.com>
2023-08-16 10:12:13 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: Write a function that prints the alphabet, in lowercase
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
