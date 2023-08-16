0ded3dab328417518c92881a0bd138ce0147296f
Rukiyenur <rokiaglal@yahoo.com>
2023-08-16 13:12:05 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#includ "main.h"
 
 /**
  * main - check the code.
@@ -7,14 +7,17 @@
  */
 int main(void)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+	int r;
+	int _islower(int c) 
+{
+    return (c >= 'a' && c <= 'z');
+}	
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
