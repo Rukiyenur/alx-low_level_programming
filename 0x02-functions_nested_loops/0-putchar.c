55d377180e7b89321d3b902434e75ce8f1516ae5
Rukiyenur <rokiaglal@yahoo.com>
2023-08-16 08:43:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: a program that prints _putchar
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	putchar('_putchar');
+	return (0);
+}
